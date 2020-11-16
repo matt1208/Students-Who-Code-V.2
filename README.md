35f5a8590381e68e5003dcf09280dc0348e1790a
Matthew <matthewvandyke08@gmail.com>
2020-11-15 19:46:48 -0800

    Main Points + CC Added

--- README.md
+++ README.md
@@ -1,30 +1,21 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+## Students Who Code
+Students Who Code Website Files
+Computer Science Courses created for students by students.
 
-## Getting Started
+## V.2 
+This is Version 2 of Students Who Code. Includes the website being transfers to Next.js, new homepage design, new auth experiencem and refreshed color scheme. 
 
-First, run the development server:
+## Courses 
+Courses will include how to use the code editor, Repl.it. Plus teaching students Web Development.
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+Courses will be broken down into smaller sections so students are not overwellmed with information, and most courses will have review sections to review the skills they have alreadly learned.
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Different courses about computer science and with different coding langauges are being created.
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+## Coding Connected 
+While learning how to code, stay connected! Share the projects you have been working on to be featured. Ask any questions you have to Code Masters through a live messanging tool.
 
-## Learn More
+## Sign Up
+Sign up to use Students Who Code right now at studentswhocode.org/signup
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/import?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+V.2 is not out yet -- In Development
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ export default function Home() {
     <div className={styles.maincontainer}>
       <Head>
         <title>Students Who Code</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/src/Logo1.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <ul className={styles.navbar}>
         <Link href="/"><img src="/src/Logo2.png" alt="Home Logo" height="75px" width="75px"></img></Link>
@@ -29,6 +30,51 @@ export default function Home() {
       </div>
     </div>
 
+    <br></br>
+
+    <div className={styles.mainpoints}>
+      <div className={styles.mainpointscolumn} style={{ marginRight: "10px"}}>
+        <center><img src="src/BinaryIcon.png" alt="Code Icon" height="120px" width="120px"></img></center>
+        <p>Students changing the world with code, due to their passion for change and computer science. We have courses for students interested in Web Development with more to come!</p>
+      </div>
+      <div className={styles.mainpointscolumn} style={{ marginRight: "10px"}}>
+        <center><img src="src/MoneyIcon.png" alt="Money Icon" height="120px" width="120px"></img></center>
+        <p>Students Who Code is free for all users. We believe that no students should have to pay for computer science education, and miss there chance to create the next world changing program! </p>
+      </div>
+      <div className={styles.mainpointscolumn}>
+        <center><img src="src/BookIcon.png" alt="Book Icon" height="120px" width="120px"></img></center>
+        <p>For students by students. All courses are designed and built by  high school students.  Students can be assured that the material is understandable and user friendly.</p>
+      </div>
+    </div>
+
+    <br></br>
+
+    <div>
+      <p className={styles.header3} style={{ color: "white", textAlign: "center" }}>Coding Connected</p>
+      <p className={styles.normaltext} style={{ textAlign: "center", paddingLeft: "44px", paddingRight: "44px", fontWeight: "bold" }}>Our platform to help students grow and learn about computer science. Students Who Code wants all users and students to take part in Coding Connected.</p>
+    </div>
+
+    <div className={styles.connecetedrow}>
+        <div className={styles.connectedcolumn} data-aos="fade-down">
+          <center><img src="src/Message2.png" alt="Connected Graphic" height="100px" width="100px"></img></center>
+          <h2>Code Masters</h2>
+          <p>Code Masters are high school coders who there to help you whenever you have a question or want feeback.</p>
+          <center><Link href="login.html" target="_blank"><button className={styles.secondarybtndark}>Connect</button></Link></center>
+          <br></br>
+        </div>
+        <div className={styles.connectedcolumn} data-aos="fade-down">
+          <center><img src="src/Spotlight.png" alt="Spotlight" height="100px" width="100px"></img></center>
+          <h2>Submit A Project</h2>
+          <p>Login to submit your coding projects to be spotlighted or featured for its purpose or function!</p>
+          <center><Link href="login.html"><button className={styles.secondarybtndark}>Login</button></Link></center>
+          <br></br>
+        </div>
+    </div>
+
+
+    <script src="https://embed.small.chat/T015AE2ETNHG015AE5MT3P.js" async></script>
+
+  
        
     </div>
   )
