d9a8717e01fd600b12521a1a2bb8bc8f62ef7b3b
Matthew <matthewvandyke08@gmail.com>
2020-12-05 11:51:11 -0800

    Promo Update

--- pages/codingconnected.js
+++ pages/codingconnected.js
@@ -27,7 +27,7 @@ export default function Home() {
     <div className={styles.promo} style={{alignItems:"center"}}>
         <center><img src="/snowman.svg" alt="snowman" className={styles.promosnowman}></img></center>
         <a href="/holiday"><p className={styles.header3} style={{ color: "white",  marginBlockEnd: "10px", textDecoration: "none" }}>Holiday Challenge</p></a>
-        <a href="/holiday"><p className={styles.normaltext} style={{ fontWeight: "bold"}}>A week long challenge during the Holiday week. Your task is to create a web page where you can post your Holiday photos using web technologies. Click to Sign Up &rarr;	</p></a>
+        <a href="/holiday"><p className={styles.normaltext} style={{ fontWeight: "bold"}}>A week long challenge during the Holiday week. Your task is to create a holiday themed website using web technologies. Click to Sign Up &rarr;	</p></a>
       </div>
 
     
