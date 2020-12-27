8fac02d80c534dbf4e1719846c4e2b60523a1c30
Matthew <matthewvandyke08@gmail.com>
2020-12-27 13:18:34 -0800

    Promo Updated

--- components/promo.js
+++ components/promo.js
@@ -5,7 +5,7 @@ const Promo = () => (
     <div className={styles.promo} style={{alignItems:"center"}}>
         <center><img src="/snowman.svg" alt="snowman" className={styles.promosnowman}></img></center>
         <a href="/holiday"><p className={styles.header3} style={{ color: "white",  marginBlockEnd: "10px", textDecoration: "none" }}>Holiday Challenge</p></a>
-        <a href="/holiday"><p className={styles.normaltext} style={{ fontWeight: "bold"}}>A week long challenge during the Holiday week. Your task is to create a holiday themed website using web technologies.</p></a>
+        <a href="/holiday"><p className={styles.normaltext} style={{ fontWeight: "bold"}}>A week long challenge during the Holiday week. Check out the winners now!</p></a>
       </div>
 );
 
